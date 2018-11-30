d62bc6576b31af578988fcf2dcb7a57c42458453
Carlos Villalta <CarlosFindley@protonmail.com>
2018-11-30 00:23:16 -0800

    update

--- liri.js
+++ liri.js
@@ -17,17 +17,59 @@ var Spotify = require("node-spotify-api");
 var moment = require("moment");
 
 
-
-
-
-// You should then be able to access your keys information like so
-var spotify = new Spotify(keys.spotify);
-
+//---------------------------------------------------------------
 // Make it so liri.js can take in one of the following commands:
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
-var userCommand = process.argv[2];
+// Grabbing user action command from CLI
+var actionCommand = process.argv[2];
+// Grabbing user item command (e.g. "concert-this") from CLI.  This will grab everything after index 3 and ignore anything prior
+var userCommand = process.argv.slice(3).join(" ")
 
+
+
+// We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (action) {
+    case "total":
+      total();
+      break;
+    
+    case "deposit":
+      deposit();
+      break;
+    
+    case "withdraw":
+      withdraw();
+      break;
+    
+    case "lotto":
+      lotto();
+      break;
+    }
+
+    function liriRun(appCommand, userSearch) {
+        switch (appCommand) {
+            case "spotify-this-song":
+                getSpotify(userSearch);
+                break;
+    
+            case "concert-this":
+                getBandsInTown(userSearch);
+                break;
+    
+            case "movie-this":
+                getOMDB(userSearch);
+                break;
+    
+            case "do-what-it-says":
+                getRandom();
+                break;
+            // If appCommand is left blank, return a default message to user
+            default:
+                console.log("Please enter one of the following commands: 'concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says' in order to continue");
+        }
+    };
 
